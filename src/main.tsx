a424c7588d53577b95ab8cbe8dda88b391d2f741
thanongsaklaoketkich <thanong_777@hotmail.com>
2026-03-02 15:28:33 +0100

    Show open layers Map

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,26 @@
 // @ts-ignore
 import {createRoot} from "react-dom/client";
+import {useEffect, useRef} from "react";
+import TileLayer from "ol/layer/Tile.js";
+import {OSM} from "ol/source.js";
+import {useGeographic} from "ol/proj.js";
+import Map from "ol/Map.js";
+import View from "ol/View.js";
+
+import "ol/ol.css";
+
+useGeographic()
+
+const layers = [new TileLayer({ source: new OSM()})];
+const map = new Map({
+    view: new View({center: [10.7, 59.9], zoom: 8}),
+    layers,
+});
 
 function Application() {
-    return <div>A map Application</div>;
+    const mapRef = useRef<HTMLDivElement|null>(null);
+    useEffect(() => map.setTarget(mapRef.current!), []);
+    return <div ref={mapRef}>A map Application</div>;
 }
 
 createRoot(document.getElementById("app")!).render(<Application />);
